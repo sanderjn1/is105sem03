006c5698de07e1ff3e0108a1f844180bf5fb019f
sanderjn1 <112398478+sanderjn1@users.noreply.github.com>
2023-04-20 15:14:34 +0200

    yolo

--- mycrypt/mycrypt.go
+++ mycrypt/mycrypt.go
@@ -1,23 +1,22 @@
 package mycrypt
 
-var AlfSem03 []rune = []rune("abcdefghijklmnopqrstuvwxyzæøåABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ0123456789.,:; ")
+var ALF_SEM03 []rune = []rune("abcdefghijklmnopqrstuvwxyzæøå0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ.,:; ")
 
-// Krypter encrypts a message using a given alphabet and shift value
 func Krypter(melding []rune, alphabet []rune, chiffer int) []rune {
 	kryptertMelding := make([]rune, len(melding))
 	for i := 0; i < len(melding); i++ {
-		indeks := SokIAlfabetet(melding[i], alphabet)
+		indeks := sokIAlfabetet(melding[i], alphabet)
 		if indeks+chiffer >= len(alphabet) {
 			kryptertMelding[i] = alphabet[indeks+chiffer-len(alphabet)]
 		} else {
 			kryptertMelding[i] = alphabet[indeks+chiffer]
 		}
+
 	}
 	return kryptertMelding
 }
 
-// sokIAlfabetet finds the position of a symbol in an alphabet
-func SokIAlfabetet(symbol rune, alfabet []rune) int {
+func sokIAlfabetet(symbol rune, alfabet []rune) int {
 	for i := 0; i < len(alfabet); i++ {
 		if symbol == alfabet[i] {
 			return i
