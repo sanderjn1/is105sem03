eae2173e20b9f5f25b1bfefcab8cf7ea72b9e93d
sanderjn1 <112398478+sanderjn1@users.noreply.github.com>
2023-04-21 15:25:17 +0200

    mm

--- mycrypt/mycrypt.go
+++ mycrypt/mycrypt.go
@@ -1,35 +1,25 @@
 package mycrypt
 
-var ALF_SEM03 []rune = []rune("abcdefghijklmnopqrstuvwxyzæøå0123456789ABCDEFGHIJKLMONPQRSTUVWXYZÆØÅ.,:; ")
+var ALF_SEM03 []rune = []rune("abcdefghijklmnopqrstuvwxyzæøå0123456789.,:; KSN") //ABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ
 
 func Krypter(melding []rune, alphabet []rune, chiffer int) []rune {
-
-	modCipher := chiffer % len(ALF_SEM03)
-
-	shiftedAlphabet := shiftAlphabet(ALF_SEM03, modCipher)
-
-	lookup := make(map[rune]rune)
-
-	for i := 0; i < len(ALF_SEM03); i++ {
-		lookup[ALF_SEM03[i]] = shiftedAlphabet[i]
-	}
-
 	kryptertMelding := make([]rune, len(melding))
 	for i := 0; i < len(melding); i++ {
-		kryptertMelding[i] = lookup[melding[i]]
+		indeks := sokIAlfabetet(melding[i], alphabet)
+		if indeks+chiffer >= len(alphabet) {
+			kryptertMelding[i] = alphabet[indeks+chiffer-len(alphabet)]
+		} else {
+			kryptertMelding[i] = alphabet[indeks+chiffer]
+		}
+
 	}
 	return kryptertMelding
-}
-
-func shiftAlphabet(alphabet []rune, chiffer int) []rune {
-	return append(alphabet[chiffer:], alphabet[:chiffer]...)
 }
 
 func sokIAlfabetet(symbol rune, alfabet []rune) int {
 	for i := 0; i < len(alfabet); i++ {
 		if symbol == alfabet[i] {
 			return i
-			break
 		}
 	}
 	return -1
